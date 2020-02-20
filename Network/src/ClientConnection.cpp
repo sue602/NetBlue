66500912d28c820d5a1cca245ba49f0fd3357483
ltzd <ltzd@centos69-linux.shared>
2020-02-20 23:04:32 +0800

    随便搞2

--- Network/src/ClientConnection.cpp
+++ Network/src/ClientConnection.cpp
@@ -12,8 +12,6 @@ using Poco::Logger;
 #include "NetworkMgr.h"
 #include "MessageQueue.h"
 #include "ClientConnection.h"
-#include "Poco/ThreadPool.h"
-using Poco::ThreadPool;
 
 ClientConnection::ClientConnection(const StreamSocket& ss):
 		TCPServerConnection(ss),
@@ -37,18 +35,29 @@ ClientConnection::~ClientConnection() {
 void ClientConnection::run(){
 	StreamSocket& ss = socket();
 	_socket = & ss;
+	Poco::Timespan timeout(2,0);
+	_socket->setReceiveTimeout(timeout);
+	_socket->setBlocking(false);//设置为非阻塞
 	try
 	{
-		int n = 0;
 		unsigned short headRead = 0;
 		const unsigned short headSize = 2;
 		char buffer[headSize] = {0};//头长度
-		//接收头数据
-		n = ss.receiveBytes(buffer+headRead, headSize-headRead);
-		while (n > 0)
+		while(!_stopped)
 		{
-			headRead = headRead + n;
-			if(headSize == headRead)
+			//接收数据
+			int n = ss.receiveBytes(buffer+headRead, headSize-headRead);
+			if( n > 0)
+			{
+				headRead = headRead + n;
+			}
+			if(0 == n) //这里表示对端的socket已正常关闭
+			{
+				_stopped = 1;
+				std::cout << "client disconnect id = " << ID << std::endl;
+				break;
+			}
+			else if(headSize == headRead)
 			{
 				headRead = 0;
 				int len  = buffer[0] * 256 + buffer[1];
@@ -60,33 +69,23 @@ void ClientConnection::run(){
 					int recv = 0;
 					while (recv<(len-2))
 					{
-						int count = ss.receiveBytes(dataRev->base() + headSize,len);
-						if(0 == count) //这里表示对端的socket已正常关闭
-						{
-							_stopped = 1;
-							std::cout << "client disconnect id = " << ID << std::endl;
-							return;
-						}
+						int count = ss.receiveBytes(dataRev->base() + 2,len);
 						recv += count;
 					}
 					dataRev->setTag(ID);
 					NetworkMgr::Instance()->addClientMsg(dataRev);
-
-					//从待发送队列弹出一条消息
-					ByteArray * dataSnd = _mq->popSndMsg();
-					if( NULL != dataSnd)
-					{
-						ss.sendBytes(dataSnd->base(), dataSnd->length());
-						//回收
-						_mq->release(dataSnd);
-					}
 				}
 			}
-			 //继续读头文件
-			n = ss.receiveBytes(buffer+headRead, headSize-headRead);
+			//从待发送队列弹出一条消息
+			ByteArray * dataSnd = _mq->popSndMsg();
+			if( NULL != dataSnd)
+			{
+				ss.sendBytes(dataSnd->base(), dataSnd->length());
+				//回收
+				_mq->release(dataSnd);
+			}
 		}
-		_stopped = 1;
-		std::cout << "client disconnect id = " << ID << std::endl;
+		std::cerr << "Client : disconnected from " << _peer << std::endl;
 	}
 	catch (Poco::Exception& exc)
 	{
@@ -107,6 +106,8 @@ int ClientConnection::getID()
 void ClientConnection::sendMsg(ByteArray * msg)
 {
 	_mq->pushSndMsg(msg);
+//	_socket->sendBytes(msg->base(), msg->length());
+//	_mq->release(msg);
 }
 
 void ClientConnection::disconnect()
